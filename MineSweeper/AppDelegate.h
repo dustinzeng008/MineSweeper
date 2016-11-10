c7dc67284f0c6208318aece86a0252934cd516d4
Yong Zeng <dustinzeng0608@gmail.com>
2016-11-10 00:49:00 -0500

    Initial Commit

---
+++ MineSweeper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MineSweeper
+//
+//  Created by Yong Zeng on 11/10/16.
+//  Copyright © 2016 Yong Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MineSweeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MineSweeper
+//
+//  Created by Yong Zeng on 11/10/16.
+//  Copyright © 2016 Yong Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
